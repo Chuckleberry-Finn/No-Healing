771b3fb68e1674e8803b5669c539853f50fc9d19
Chuckleberry Finn <6071161+ChuckTheSheep@users.noreply.github.com>
2022-07-25 23:57:34 -0400

    fix not actually working

--- Contents/mods/No Healing/media/lua/client/No Healing - client.lua
+++ Contents/mods/No Healing/media/lua/client/No Healing - client.lua
@@ -14,14 +14,23 @@ local function noHeal(player)
         local bpIndex = BodyPart:getIndex()+1
 
         noHealingRecord[player][bpIndex] = noHealingRecord[player][bpIndex] or {}
-        noHealingRecord[player][bpIndex]["lastScratchTime"] = math.max((noHealingRecord[player][bpIndex]["lastScratchTime"] or 0), BodyPart:getScratchTime())
-        noHealingRecord[player][bpIndex]["lastCutTime"] = math.max((noHealingRecord[player][bpIndex]["lastCutTime"] or 0), BodyPart:getCutTime())
-        noHealingRecord[player][bpIndex]["lastBiteTime"] = math.max((noHealingRecord[player][bpIndex]["lastBiteTime"] or 0), BodyPart:getBiteTime())
-        noHealingRecord[player][bpIndex]["lastBleedingTime"] = math.max((noHealingRecord[player][bpIndex]["lastBleedingTime"] or 0), BodyPart:getBleedingTime())
-        noHealingRecord[player][bpIndex]["lastBurnTime"] = math.max((noHealingRecord[player][bpIndex]["lastBurnTime"] or 0), BodyPart:getBurnTime())
-        noHealingRecord[player][bpIndex]["lastFractureTime"] = math.max((noHealingRecord[player][bpIndex]["lastFractureTime"] or 0), BodyPart:getFractureTime())
-        noHealingRecord[player][bpIndex]["lastDeepWoundTime"] = math.max((noHealingRecord[player][bpIndex]["lastDeepWoundTime"] or 0), BodyPart:getDeepWoundTime())
 
+        noHealingRecord[player][bpIndex]["lastHealth"] = math.min((noHealingRecord[player][bpIndex]["lastHealth"] or BodyPart:getHealth()), BodyPart:getHealth())
+        BodyPart:SetHealth(noHealingRecord[player][bpIndex]["lastHealth"])
+        noHealingRecord[player][bpIndex]["lastScratchTime"] = math.max((noHealingRecord[player][bpIndex]["lastScratchTime"] or 0), BodyPart:getScratchTime())
+        BodyPart:setScratchTime(noHealingRecord[player][bpIndex]["lastScratchTime"])
+        noHealingRecord[player][bpIndex]["lastCutTime"] = math.max((noHealingRecord[player][bpIndex]["lastCutTime"] or 0), BodyPart:getCutTime())
+        BodyPart:setCutTime(noHealingRecord[player][bpIndex]["lastCutTime"])
+        noHealingRecord[player][bpIndex]["lastBiteTime"] = math.max((noHealingRecord[player][bpIndex]["lastBiteTime"] or 0), BodyPart:getBiteTime())
+        BodyPart:setBiteTime(noHealingRecord[player][bpIndex]["lastBiteTime"])
+        noHealingRecord[player][bpIndex]["lastBleedingTime"] = math.max((noHealingRecord[player][bpIndex]["lastBleedingTime"] or 0), BodyPart:getBleedingTime())
+        BodyPart:setBleedingTime(noHealingRecord[player][bpIndex]["lastBleedingTime"])
+        noHealingRecord[player][bpIndex]["lastBurnTime"] = math.max((noHealingRecord[player][bpIndex]["lastBurnTime"] or 0), BodyPart:getBurnTime())
+        BodyPart:setBurnTime(noHealingRecord[player][bpIndex]["lastBurnTime"])
+        noHealingRecord[player][bpIndex]["lastFractureTime"] = math.max((noHealingRecord[player][bpIndex]["lastFractureTime"] or 0), BodyPart:getFractureTime())
+        BodyPart:setFractureTime(noHealingRecord[player][bpIndex]["lastFractureTime"])
+        noHealingRecord[player][bpIndex]["lastDeepWoundTime"] = math.max((noHealingRecord[player][bpIndex]["lastDeepWoundTime"] or 0), BodyPart:getDeepWoundTime())
+        BodyPart:setDeepWoundTime( noHealingRecord[player][bpIndex]["lastDeepWoundTime"])
     end
 end
 Events.OnPlayerUpdate.Add(noHeal)
